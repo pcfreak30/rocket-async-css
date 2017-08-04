19c0b6dd115908cd10b115934e0e1f506cbc9330
Derrick Hammer <derrick@derrickhammer.com>
2017-08-04 16:05:20 -0400

    *Never cache slider *Print dummy link to satisfy layerslider style check *Add phpdocs

--- lib/Rocket/Async/CSS/Integration/LayerSlider.php
+++ lib/Rocket/Async/CSS/Integration/LayerSlider.php
@@ -4,19 +4,42 @@
 namespace Rocket\Async\CSS\Integration;
 
 
+/**
+ * Class LayerSlider
+ * @package Rocket\Async\CSS\Integration
+ */
+/**
+ * Class LayerSlider
+ * @package Rocket\Async\CSS\Integration
+ */
 class LayerSlider implements IntegrationInterface {
 
+	/**
+	 * @var string
+	 */
 	private $default_skin;
+	/**
+	 * @var array
+	 */
+	private $skins_queue;
 
+	/**
+	 *
+	 */
 	public function init() {
 		if ( class_exists( 'LS_Sliders' ) ) {
+			add_filter( 'ls_use_cache', '__return_false' );
 			add_filter( 'layerslider_pre_parse_defaults', [ $this, 'set_default_skin' ], PHP_INT_MAX );
 			add_filter( 'ls_parse_defaults', [ $this, 'enqueue_skin' ], PHP_INT_MAX );
-			add_action( 'rocket_async_css_activate', [ $this, 'purge_slider_cache' ] );
-			add_action( 'activate_LayerSlider/layerslider.php', [ $this, 'purge_slider_cache' ] );
+			add_filter( 'wp_footer', [ $this, 'print_skins' ] );
 		}
 	}
 
+	/**
+	 * @param $defaults
+	 *
+	 * @return mixed
+	 */
 	public function set_default_skin( $defaults ) {
 		$this->default_skin             = $defaults['properties']['skin'];
 		$defaults['properties']['skin'] = null;
@@ -24,6 +47,11 @@ class LayerSlider implements IntegrationInterface {
 		return $defaults;
 	}
 
+	/**
+	 * @param $slider
+	 *
+	 * @return mixed
+	 */
 	public function enqueue_skin( $slider ) {
 
 		if ( empty( $slider['attr']['skin'] ) ) {
@@ -32,16 +60,20 @@ class LayerSlider implements IntegrationInterface {
 			$skin                   = $slider['attr']['skin'];
 			$slider['attr']['skin'] = '';
 		}
-		wp_enqueue_style( "layerslider-skin-{$skin}", \LS_Sources::urlForSkin( $skin ) . '/skin.css' );
+		$this->skins_queue[] = $skin;
+		wp_enqueue_style( "layerslider-skin-{$skin}", \LS_Sources::urlForSkin( $skin ) . 'skin.css' );
 
 		return $slider;
 	}
 
-	public function purge_slider_cache() {
-		\LS_Sliders::find( [ 'limit' => 0 ] );
-		$sliders = \LS_Sliders::find( [ 'limit' => \LS_Sliders::count() ] );
-		foreach ( $sliders as $slider ) {
-			delete_transient( 'ls-slider-data-' . $slider['id'] );
-		}
+	/**
+	 *
+	 */
+	public function print_skins() {
+		foreach ( $this->skins_queue as $skin ) :
+			?>
+            <link href="<?php echo \LS_Sources::urlForSkin( $skin ) . 'skin.css' ?>"/>
+			<?php
+		endforeach;
 	}
 }
