e17c3f30c06d53c4e6bd5c5bad9d9c9a1075dd42
Derrick Hammer <derrick@derrickhammer.com>
2017-11-30 19:57:41 -0500

    *Dequeue a bunch of useless wp-rocket hooks that over eagerly purge cache if woocommerce is on

--- lib/Rocket/Async/CSS/Integration/Woocommerce.php
+++ lib/Rocket/Async/CSS/Integration/Woocommerce.php
@@ -11,8 +11,19 @@ class Woocommerce extends ComponentAbstract {
 	 *
 	 */
 	public function init() {
-		if ( class_exists( '\WooCommerce' ) && method_exists( 'WC_Cache_Helper', 'set_nocache_constants' ) ) {
-			remove_filter( 'nocache_headers', [ 'WC_Cache_Helper', 'set_nocache_constants' ] );
+		if ( class_exists( '\WooCommerce' ) ) {
+			remove_action( 'user_register', 'rocket_clean_domain' );
+			remove_action( 'profile_update', 'rocket_clean_domain' );
+			remove_action( 'deleted_user', 'rocket_clean_domain' );
+			remove_action( 'create_term', 'rocket_clean_domain' );
+			remove_action( 'edited_terms', 'rocket_clean_domain' );
+			remove_action( 'delete_term', 'rocket_clean_domain' );
+			remove_action( 'add_link', 'rocket_clean_domain' );
+			remove_action( 'edit_link', 'rocket_clean_domain' );
+			remove_action( 'delete_link', 'rocket_clean_domain' );
+			if ( method_exists( 'WC_Cache_Helper', 'set_nocache_constants' ) ) {
+				remove_filter( 'nocache_headers', [ 'WC_Cache_Helper', 'set_nocache_constants' ] );
+			}
 		}
 	}
 }
