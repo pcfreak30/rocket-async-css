c86f40d8d77faf616e41d6c0af858dbf44312e23
Derrick Hammer <derrick@derrickhammer.com>
2017-11-30 19:55:57 -0500

    *Move is_lazyload_enabled method to util component

--- lib/Rocket/Async/CSS/Util.php
+++ lib/Rocket/Async/CSS/Util.php
@@ -37,4 +37,18 @@ class Util extends ComponentAbstract {
 	public function encode_script( $data ) {
 		return base64_encode( json_encode( $data ) );
 	}
+
+	public function is_lazyload_enabled() {
+		global $a3_lazy_load_global_settings;
+		$lazy_load = false;
+		if ( class_exists( 'A3_Lazy_Load' ) ) {
+			$lazy_load = (bool) $this->a3_lazy_load_global_settings['a3l_apply_lazyloadxt'] && apply_filters( 'a3_lazy_load_run_filter', true );
+		}
+		if ( class_exists( 'LazyLoadXT' ) ) {
+			$lazy_load = true;
+		}
+
+		return $lazy_load;
+	}
+
 }
