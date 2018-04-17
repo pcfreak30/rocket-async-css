f047dc394b0a5284bd5f9a0875717dff81888c93
Derrick Hammer <derrick@derrickhammer.com>
2018-04-16 22:31:22 -0400

    *Disable lazy loading completely for all revslider images

--- lib/Rocket/Async/CSS/Integration/RevolutionSlider.php
+++ lib/Rocket/Async/CSS/Integration/RevolutionSlider.php
@@ -12,11 +12,22 @@ use ComposePress\Core\Abstracts\Component;
  */
 class RevolutionSlider extends Component {
 
+	/**
+	 * @var bool
+	 */
+	private $in_slider = false;
+
+
+	/**
+	 *
+	 */
 	public function init() {
 		if ( shortcode_exists( 'rev_slider' ) ) {
 			remove_shortcode( 'rev_slider' );
 			add_shortcode( 'rev_slider', array( $this, 'rev_slider_compatibility' ) );
 			add_filter( 'rocket_async_css_lazy_load_responsive_image', [ $this, 'check_image' ], 10, 4 );
+			add_action( 'revslider_modify_core_settings', [ $this, 'in_slider' ] );
+			add_filter( 'revslider_add_js_delay', [ $this, 'out_slider' ], 10, 2 );
 		}
 	}
 
@@ -40,9 +51,27 @@ class RevolutionSlider extends Component {
 	 * @return bool
 	 */
 	public function check_image( $value, $classes, $src, $image ) {
-		if ( in_array( 'rev-slidebg', $classes ) ) {
+		if ( $this->in_slider || in_array( 'rev-slidebg', $classes ) ) {
 			$value = false;
 		}
+
+		return $value;
+	}
+
+	/**
+	 *
+	 */
+	public function maybe_in_slider() {
+		$this->in_slider = true;
+	}
+
+	/**
+	 * @param $value
+	 *
+	 * @return mixed
+	 */
+	public function maybe_out_slider( $value ) {
+		$this->in_slider = false;
 
 		return $value;
 	}
