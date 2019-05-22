7061bcf9c716b9bd67eba606bc455a2827a6ff6f
Derrick Hammer <derrick@derrickhammer.com>
2019-05-21 20:54:02 -0400

    *Add integration with webpexpress to convert images referenced to webp format

---
+++ lib/Rocket/Async/CSS/Integration/WebPExpress.php
@@ -0,0 +1,128 @@
+<?php
+
+
+namespace Rocket\Async\CSS\Integration;
+
+
+use ComposePress\Core\Abstracts\Component;
+use WebPExpress\AlterHtmlImageUrls;
+use WebPExpress\Option;
+
+/**
+ * Class WebPExpress
+ * @package Rocket\Async\CSS\Integration
+ */
+class WebPExpress extends Component {
+
+	/**
+	 * @var bool
+	 */
+	private $conditional = false;
+
+	/**
+	 * @var AlterHtmlImageUrls
+	 */
+	private $image_replace;
+	private $webp_available = false;
+
+	/**
+	 *
+	 */
+	public function init() {
+		if ( class_exists( '\WebPExpress\Config' ) && Option::getOption( 'webp-express-alter-html', false ) ) {
+			$this->webp_available = true;
+			$options              = json_decode( Option::getOption( 'webp-express-alter-html-options', null ), true );
+			if ( 'url' === Option::getOption( 'webp-express-alter-html-replacement' ) && $options['only-for-webp-enabled-browsers'] ) {
+				$this->conditional = true;
+			}
+
+			$autoload = WEBPEXPRESS_PLUGIN_DIR . '/vendor/autoload.php';
+			if ( ! $this->plugin->wp_filesystem->is_file( $autoload ) ) {
+				return;
+			}
+
+			require_once $autoload;
+
+			if ( ! class_exists( '\WebPExpress\AlterHtmlImageUrls' ) ) {
+				return;
+			}
+
+			$this->image_replace = new AlterHtmlImageUrls;
+
+			add_filter( 'rocket_async_css_after_process_images', [ $this, 'maybe_process' ], 10, 2 );
+		}
+	}
+
+	public function maybe_process( $css, $url = null ) {
+		if ( ! $url ) {
+			$url = home_url( $_SERVER['REQUEST_URI'] );
+		}
+
+		if ( ( $this->conditional && false !== strpos( $_SERVER['HTTP_ACCEPT'], 'image/webp' ) ) || ! $this->conditional ) {
+			$css = $this->process( $css, $url );
+		}
+		return $css;
+	}
+
+	/**
+	 * @param $css
+	 * @param $url
+	 *
+	 * @return mixed
+	 */
+	private function process( $css, $url ) {
+
+		if ( ! apply_filters( 'rocket_async_css_webpexpress_process', true, $css, $url ) ) {
+			return $css;
+		}
+
+		preg_match_all( '/url\\(\\s*([\'"]?(.*?)[\'"]?|[^\\)\\s]+)\\s*\\)/', $css, $matches );
+		//Ensure there are matches
+		if ( ! empty( $matches ) && ! empty( $matches[1] ) ) {
+			foreach ( $matches[2] as $index => $match ) {
+				if ( empty( $match ) ) {
+					$match = $matches[1][ $index ];
+				}
+				if ( 0 === strpos( $match, 'data:' ) ) {
+					continue;
+				}
+				$match       = trim( $match, '"' . "'" );
+				$fixed_match = $match;
+				if ( 0 === strpos( $fixed_match, '//' ) ) {
+					//Handle no protocol urls
+					$fixed_match = rocket_add_url_protocol( $fixed_match );
+				}
+				$url_parts = parse_url( $match );
+				if ( empty( $url_parts['host'] ) ) {
+					$fixed_match = \phpUri::parse( $url )->join( $fixed_match );
+					$url_parts   = parse_url( $fixed_match );
+				}
+				if ( $url_parts['host'] != $this->plugin->domain && ! in_array( $url_parts['host'], $this->plugin->cdn_domains ) ) {
+					continue;
+				}
+				$url_parts['host'] = $this->plugin->domain;
+
+				$final_url = http_build_url( $url_parts );
+				$final_url = $this->image_replace->replaceUrl( $final_url );
+
+				if ( ! $final_url ) {
+					$final_url = $match;
+				} else {
+					$final_url = get_rocket_cdn_url( $final_url, [ 'images', 'all' ] );
+				}
+
+				$css_part = str_replace( $match, $final_url, $matches[0][ $index ] );
+				$css      = str_replace( $matches[0][ $index ], $css_part, $css );
+			}
+		}
+
+		return $css;
+	}
+
+	/**
+	 * @return bool
+	 */
+	public function is_webp_available() {
+		return $this->webp_available;
+	}
+}
