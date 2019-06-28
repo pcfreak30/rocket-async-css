bfb41ad9f74d9d1fe5fa708eb9738924f4471009
Derrick Hammer <derrick@derrickhammer.com>
2019-06-28 18:55:51 -0400

    *Add font-display support for divi etmodules font

--- lib/Rocket/Async/CSS/Integration/Divi.php
+++ lib/Rocket/Async/CSS/Integration/Divi.php
@@ -14,10 +14,21 @@ class Divi extends Component {
 		if ( function_exists( 'et_setup_theme' ) ) {
 			add_filter( 'et_get_option_divi_minify_combine_styles', [ $this, 'return_false' ] );
 			add_filter( 'et_load_unminified_styles', '__return_false' );
+			add_filter( 'rocket_async_css_font_display', [ $this, 'process_font' ], 10, 2 );
 		}
 	}
 
 	public function return_false() {
 		return 'false';
 	}
+
+	public function process_font( $mode, $rules ) {
+		$family = $rules['font-family'];
+		$family = strtolower( $family );
+		if ( 'etmodules' === $family ) {
+			$mode = 'block';
+		}
+		return $mode;
+	}
+
 }
