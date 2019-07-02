ee6aa22e1e57493c6c40438f7f9afb0c4dfc27dc
Derrick Hammer <derrick@derrickhammer.com>
2019-07-02 02:54:52 -0400

    *Fix variable name

--- lib/Rocket/Async/CSS.php
+++ lib/Rocket/Async/CSS.php
@@ -1060,7 +1060,7 @@ class CSS extends Plugin {
 		$filenames
 	) {
 		if ( empty( $this->cache ) ) {
-			$data = [ 'filename' => $filename, 'remote_file_list' => $this->remote_file_list ];
+			$data = [ 'filename' => $filenames, 'remote_file_list' => $this->remote_file_list ];
 			foreach ( $filenames as $media => $filename ) {
 				$this->put_content( $filename, apply_filters( 'rocket_async_css_output', $this->css[ $media ], $media ) );
 				$data['href'][ $media ] = get_rocket_cdn_url( set_url_scheme( str_replace( WP_CONTENT_DIR, WP_CONTENT_URL, $filename ) ), [
