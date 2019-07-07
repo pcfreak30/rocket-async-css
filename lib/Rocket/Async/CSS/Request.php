797492e9221cba1d13f5dc10011655cd9446802c
Derrick Hammer <derrick@derrickhammer.com>
2019-07-07 16:39:56 -0400

    *Force minify_concatenate_css option to false to shortcut some hooks like elementor purging

--- lib/Rocket/Async/CSS/Request.php
+++ lib/Rocket/Async/CSS/Request.php
@@ -28,6 +28,7 @@ class Request extends Component {
 			add_filter( 'pre_get_rocket_option_async_css', '__return_zero' );
 			add_action( 'wp_footer', [ $this, 'scripts' ], PHP_INT_MAX );
 		}
+		add_filter( 'pre_get_rocket_option_minify_concatenate_css', '__return_zero' );
 
 	}
 
